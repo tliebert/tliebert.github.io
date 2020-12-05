77e9ed9ab83cd5cbfe5a00e0b302e26ab8f677f2
Thomas Liebert <thomas.d.liebert@gmail.com>
2020-12-05 14:47:02 -0600

    fixed active player toggle module

--- scripts.js
+++ scripts.js
@@ -9,16 +9,47 @@ const makePlayer = function(name, token) {
 // gameBoard module 
 
 const gameBoard = (function() {
-    let boardArray = Array.from({length:9}, () => undefined)
+    let boardArray = Array.from({length:9}, () => undefined);
+
     const addMove = function(squareNumber) {
-        boardArray[squareNumber] = x;
+        boardArray[squareNumber] = players.getActive().token;
+        players.switchActive();
+        gameController.renderBoard();
     }
+
     return {
         boardArray, addMove
     }
 })()
 
-//demo players
+//Player creation and active player control 
+
+
+const players = (function(){
+    let playerList = [];
+
+    let activePlayer = playerList[0]
+
+    const getActive = function() {
+        return activePlayer
+    }
+
+    const switchActive = function(){
+        if (activePlayer === playerList[0]) {
+            activePlayer = playerList[1];
+        }
+        else {
+            activePlayer = playerList[0];
+        }
+        return activePlayer
+    };
+
+    const makePlayer = function(name, token) {
+        playerList.push({name, token})
+    }
+    return {switchActive, makePlayer, getActive, playerList}
+})()
+
 
 // main game controller 
 
@@ -27,20 +58,14 @@ const gameController = (function() {
     const boardContainer = document.getElementById("boardContainer");
 
     const renderBoard = function(){
-        console.log(gameBoard.boardArray)
+        removeChildren(boardContainer)
         gameBoard.boardArray.forEach(addNode);
     }
 
-    const appendNodeToContainer = function(node, container, index) {
-        node.setAttribute("data-square", index);
-        node.classList.add("squareBox");
-        container.appendChild(node);
-    }
-
-    const addEvent = function(node) {
-        const selectedSquare = node.dataset.dataSquare;
-        const playerToken = 
-        node.addEventListener("click", gameBoard.addMove.bind(gameBoard, selectedSquare))
+    const removeChildren = function(parent) {
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild)
+        }
     }
 
     const addNode = function(el, indexInArray) {
@@ -50,11 +75,25 @@ const gameController = (function() {
         }
         else {
             let filledNode = document.createElement("div");
-            filledNode.textContent = el;
+            filledNode.textContent = el
             appendNodeToContainer(filledNode, boardContainer, indexInArray);
         }
 
     }
+
+    const appendNodeToContainer = function(node, container, index) {
+        node.setAttribute("data-square", index);
+        node.classList.add("squareBox");
+        addListener(node);
+        container.appendChild(node);
+    }
+
+    const addListener = function(node) {
+        const selectedSquare = node.getAttribute("data-square");
+        node.addEventListener("click", gameBoard.addMove.bind(gameBoard, selectedSquare));
+    }
+
+
     return {renderBoard}
 })()
 
